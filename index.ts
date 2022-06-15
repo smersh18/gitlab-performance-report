aca7ae198ade1341a65a558bfd2d32ddbfccbfe2
dmitriy <d.12.09.2002.d@gmail.com>
2022-06-15 11:56:40 +0300

    тестовая функция вычисления рабочих часов

--- index.ts
+++ index.ts
@@ -1,1 +1,59 @@
-console.log('Hello typescript \!')
+import moment from 'moment'
+
+function isWorkday(date: any): boolean {
+    const dayOfWeek: number = date.day()
+    return dayOfWeek !== 0 && dayOfWeek !== 6
+}
+
+function getWorkingHours(fromDate: string, toDate: string): number | string {
+    let workHours: number = 0
+    let from: any = moment(fromDate)
+    let to: any = moment(toDate)
+    if (from.isSame(to, "day")) {
+        if (to.hour() < from.hour()) {
+            return "error"
+        }
+        if (from.hour() < 9 && to.hour() < 9) {
+            return 0
+        } else {
+            if (to.hour() - from.hour() > 8) {
+                return 8
+            } else {
+                return to.hour() - from.hour()
+            }
+        }
+    }
+    if (from.hour() >= 18) {
+        workHours = workHours + 0
+    } else if (isWorkday(from) === true) {
+        if (from.hour() <= 9) {
+            workHours = workHours + 8
+        } else {
+            workHours = workHours + 18 - from.hour()
+        }
+    }
+    from.add(1, "day")
+    while (from.isSame(to, "day") === false) {
+        if (isWorkday(from) === true) {
+            workHours = workHours + 8
+        }
+        from.add(1, "day")
+    }
+    if (isWorkday(to) === true) {
+        if (to.hour() <= 9) {
+            workHours = workHours + 0
+        } else if (to.hour() >= 18) {
+            workHours = workHours + 8
+        } else if (to.hour() >= 9) {
+            workHours = workHours + to.hour() - 9
+        }
+    }
+    return workHours
+}
+
+const hours1 =
+    getWorkingHours(
+        '2022-06-10T23:00:00+03:00',
+        '2022-06-15T01:59:59+03:00',
+    )
+console.log(hours1);
