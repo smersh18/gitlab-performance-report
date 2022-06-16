fd17ebe082580137ec4d680c115c8c9f14e6e127
dmitriy <d.12.09.2002.d@gmail.com>
2022-06-16 15:39:59 +0300

    напиал тест getTimes

---
+++ src/getTimes.ts
@@ -0,0 +1,9 @@
+function getTimes(data: any) {
+    const day1: number = data.getDate().toString().padStart(2, '0')
+    const month1: number = (data.getMonth() + 1).toString().padStart(2, '0')
+    const year1: number = data.getFullYear().toString().substring(2)
+    const hours1: number = data.getHours().toString().padStart(2, '0')
+    const minutes1: number = data.getMinutes().toString().padStart(2, '0')
+    return `${day1}.${month1}.${year1} ${hours1}:${minutes1}`
+}
+export default getTimes
