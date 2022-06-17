a1585cc8f49d1ee4529a106b272f61cd8bde1edb
Vasiliy L <v.lukoyanov@gmail.com>
2022-06-17 12:41:09 +0300

    Added new test style for jest, added types in several places, removed old code

--- src/isWorkday.ts
+++
@@ -1,6 +0,0 @@
-function isWorkDay(date: any): any {
-    const dayOfWeek: number = date.day()
-    return dayOfWeek !== 0 && dayOfWeek !== 6
-}
-
-export default isWorkDay
--- src/workHours.ts
+++ src/workHours.ts
@@ -1,54 +1,54 @@
-import moment from 'moment'
+import moment, {Moment} from 'moment'
 
-function isWorkday(date: any): boolean {
-    const dayOfWeek: number = date.day()
-    return dayOfWeek !== 0 && dayOfWeek !== 6
+function isWorkDay(date: Moment): boolean {
+  const dayOfWeek: number = date.day()
+  return dayOfWeek !== 0 && dayOfWeek !== 6
 }
 
 function getWorkingHours(fromDate: string, toDate: string): number | string {
-    let workHours: number = 0
-    let from: any = moment(fromDate)
-    let to: any = moment(toDate)
-    if (from.isSame(to, "day")) {
-        if (to.hour() < from.hour()) {
-            return "error"
-        }
-        if (from.hour() < 9 && to.hour() < 9) {
-            return 0
-        } else {
-            if (to.hour() - from.hour() > 8) {
-                return 8
-            } else {
-                return to.hour() - from.hour()
-            }
-        }
+  let workHours: number = 0
+  let from: Moment = moment(fromDate)
+  let to: Moment = moment(toDate)
+
+  if (from.isSame(to, 'day')) {
+    if (to.hour() < from.hour()) {
+      return 'error'
     }
-    if (from.hour() >= 18) {
-        workHours = workHours + 0
-    } else if (isWorkday(from) === true) {
-        if (from.hour() <= 9) {
-            workHours = workHours + 8
-        } else {
-            workHours = workHours + 18 - from.hour()
-        }
+    if (from.hour() < 9 && to.hour() < 9) {
+      return 0
+    } else {
+      if (to.hour() - from.hour() > 8) {
+        return 8
+      } else {
+        return to.hour() - from.hour()
+      }
     }
-    from.add(1, "day")
-    while (from.isSame(to, "day") === false) {
-        if (isWorkday(from) === true) {
-            workHours = workHours + 8
-        }
-        from.add(1, "day")
+  }
+
+  if (from.hour() < 18 && isWorkDay(from)) {
+    if (from.hour() <= 9) {
+      workHours = workHours + 8
+    } else {
+      workHours = workHours + 18 - from.hour()
     }
-    if (isWorkday(to) === true) {
-        if (to.hour() <= 9) {
-            workHours = workHours + 0
-        } else if (to.hour() >= 18) {
-            workHours = workHours + 8
-        } else if (to.hour() >= 9) {
-            workHours = workHours + to.hour() - 9
-        }
+  }
+
+  from.add(1, 'day')
+  while (!from.isSame(to, 'day')) {
+    if (isWorkDay(from)) {
+      workHours = workHours + 8
     }
-    return workHours
+    from.add(1, 'day')
+  }
+
+  if (isWorkDay(to)) {
+    if (to.hour() > 9 && to.hour() >= 18) {
+      workHours = workHours + 8
+    } else if (to.hour() >= 9) {
+      workHours = workHours + to.hour() - 9
+    }
+  }
+  return workHours
 }
 
-export default getWorkingHours
+export {getWorkingHours, isWorkDay};
