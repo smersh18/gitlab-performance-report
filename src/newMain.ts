1c7aecca4a39073fa29745681d8cc4c9cc2d6cc1
dmitriy <d.12.09.2002.d@gmail.com>
2022-06-20 19:29:46 +0300

    исправил ошибки синтаксиса , но столкнулся с другой ошибкой

--- src/newMain.ts
+++ src/newMain.ts
@@ -1,14 +1,18 @@
-import yargs from 'yargs/yargs';
-const gql = require("graphql-tag");
-const apolloClient = require("apollo-client").ApolloClient;
-const createHttpLink = require("apollo-link-http").createHttpLink;
-const inMemoryCache = require("apollo-cache-inmemory").InMemoryCache;
-function getOptions() {
-    return yargs
-        .usage("Usage: -n <name>")
-        .option("a", {alias: "api", describe: "Your api key", type: "Integer", demandOption: true})
-        .argv
-}
+// import yargs, {Argv} from 'yargs';
+import gql from 'graphql-tag';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import ApolloClient from 'apollo-client';
+import fetch from "node-fetch";
+
+// function getOptions() {
+//     let argv = yargs
+//         .usage("Usage: -n <name>")
+//         .option("a", {alias: "api", describe: "Your api key", type: "string", demandOption: true})
+//         .argv
+// return argv
+// }
+const apiKey: any = "glpat-AePyJyMCyksNgsYGYQQV"
 async function getProjects(apiKey: string){
     const getProjects: string =
         `query {
@@ -35,14 +39,14 @@ async function request(query: string, apiKey: string) {
     });
     return result
 }
-const httpLink = createHttpLink({
+const httpLink1 = new HttpLink({
     uri: "https://git.mnxsc.tech:444/api/graphql",
-    fetch: fetch
+    fetchOptions: fetch
 });
-const client = new apolloClient({
-    link: httpLink,
-    cache: new inMemoryCache()
+const client = new ApolloClient({
+    link: httpLink1,
+    cache: new InMemoryCache()
 });
-const options: any = getOptions()
-const apiKey: any = options.api
+// const options: any = getOptions()
+// const apiKey: any = options.api
 getProjects(apiKey)
