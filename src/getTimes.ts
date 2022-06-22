d5001d41fb612436d159071ffd8f97dce1be0094
dmitriy <d.12.09.2002.d@gmail.com>
2022-06-22 16:17:56 +0300

    переписал функцию  getTimes с помощью prettyDate

--- src/getTimes.ts
+++ src/getTimes.ts
@@ -1,12 +1,10 @@
+import prettyDate from "./prettyDate";
+
 function getTimes(data: any) {
+  const hours1: number = data.getHours().toString().padStart(2, '0')
+  const minutes1: number = data.getMinutes().toString().padStart(2, '0')
 
-  const day1: number = data.getDate().toString().padStart(2, '0')
-    const month1: number = (data.getMonth() + 1).toString().padStart(2, '0')
-    const year1: number = data.getFullYear().toString().substring(2)
-    const hours1: number = data.getHours().toString().padStart(2, '0')
-    const minutes1: number = data.getMinutes().toString().padStart(2, '0')
-
-    return `${day1}.${month1}.${year1} ${hours1}:${minutes1}`
+  return `${prettyDate(data)} ${hours1}:${minutes1}`
 }
 
 export default getTimes
