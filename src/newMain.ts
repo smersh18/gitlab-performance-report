bf855a632ca7f5db73d97dcd487b3dd0b6a1d749
dmitriy <d.12.09.2002.d@gmail.com>
2022-06-23 18:06:11 +0300

    добавил новую функцию

--- src/newMain.ts
+++ src/newMain.ts
@@ -53,12 +53,36 @@ async function getMergeRequestsIds(projectFullPath: any, to: string, from: strin
         }
       }`
     const data = await request(getMergeRequestIds, apiKey);
-    console.log(data)
+    return data.project.mergeRequests.nodes.map(x => x.iid);
+}
+
+async function getMergeRequestInfo(projectFullPath: any, mrId: string, apiKey: string) {
+    const getMergeInfo =
+        `query {
+          project(fullPath: "${projectFullPath}") {
+          fullPath
+             mergeRequest(iid: "${mrId}") {
+               title
+               createdAt
+               state
+               description
+               webUrl
+            diffStatsSummary {
+                changes
+                additions
+                deletions
+                fileCount
+            }
+             }
+           }
+       }`
+    const data = await request(getMergeInfo, apiKey);
+    console.log(data.project.mergeRequest);
 }
 
 const httpLink1 = new HttpLink({
     uri: "https://git.mnxsc.tech:444/api/graphql",
-    fetch
+    fetchOptions: fetch
 });
 const client = new ApolloClient({
     link: httpLink1,
@@ -69,9 +93,15 @@ const apiKey: any = options.api
 const from: string = "2021-03-09T14:58:50+00:00"
 const to: string = "2021-04-09T14:58:50+00:00"
 const user: string = "lukoyanov"
-const branch: string = "lukoyanov"
-async function main(){
-    for(let project of (await getProjects(apiKey))){
-        console.log(getMergeRequestsIds(project.fullPath, from, to, apiKey) }
+const branch: string = "master"
+
+async function main() {
+    for (const project of (await getProjects(apiKey))) {
+        const mergeRequestIds = await getMergeRequestsIds(project.fullPath, from, to, apiKey)
+        for (const mrId of mergeRequestIds) {
+            getMergeRequestInfo(project.fullPath, mrId, apiKey)
+        }
+    }
 }
+
 main()
